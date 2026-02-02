d4cfd075da819607315d382cd1482f2d83d93265
Chengjie Peng Lin <chengjiepenglin@MacBook-Pro-de-Capitole.local>
2026-02-02 09:56:03 +0100

    update year

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer({ username }: FooterProps) {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <p className="text-sm text-muted-foreground">
-              © {currentYear} Chengjie Peng Lin. All rights reserved.
+              Chengjie Peng Lin
             </p>
           </div>
 
